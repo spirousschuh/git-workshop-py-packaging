a7e39465d629a0c438c576394ec53d8a1536c2d4
ch.lange <christoph.lange@tu-berlin.de>
2021-03-18 11:34:42 +0100

    add a mock package

---
+++ git-workshop-py-packaging/__init__.py
@@ -0,0 +1,4 @@
+from git-workshop-py-packaging import numbers_magic
+
+
+__all__ = ['numbers_magic']
---
+++ setup.py
@@ -0,0 +1,38 @@
+import io
+import os
+import re
+import setuptools
+
+
+def read(filename):
+    filename = os.path.join(os.path.dirname(__file__), filename)
+    text_type = type(u"")
+    with io.open(filename, mode="r", encoding='utf-8') as fd:
+        return re.sub(
+            text_type(r':[a-z]+:`~?(.*?)`'),
+            text_type(r'``\1``'),
+            fd.read()
+        )
+
+
+setuptools.setup(
+    name="git-workshop-py-packaging",
+    version="0.0.1",
+    url="https://github.com/spirousschuh/git-workshop-py-packaging",
+    license='MIT',
+
+    author="Christoph Lange",
+    author_email="christoph.lange@tu-berlin.de",
+
+    description="Some tasks to practise python packaging",
+    long_description=read("README.rst"),
+    long_description_content_type='test/x-rst',
+    packages=setuptools.find_packages(exclude=('tests')),
+    install_requires=['click'],
+    entry_points={
+        'console_scripts': [
+            'do_nothing='
+            'git-workshop-py-packaging.cli:cli_group'
+        ],
+    },
+)
